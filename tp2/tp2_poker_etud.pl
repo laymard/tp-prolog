ec4d3a815a83941c6bd929631f4ac80200c7a7e4
bseznec <blandine.seznec@insa-rennes.fr>
2015-12-01 23:43:41 +0100

    tp2 fini

--- tp2/tp2_poker_etud.pl
+++ tp2/tp2_poker_etud.pl
@@ -109,6 +109,7 @@ est_main(main(C1,C2,C3,C4,C5)) :-
 % ==============================================================================
 %       QUESTION 3 : inf_carte(C1,C2) première version
 % ============================================================================= 
+/*inf_hauteur : compare la hauteur de deux carte*/
 inf_hauteur(Haut1,Haut2) :-
 	succ_hauteur(Haut1,Haut2).
 
@@ -116,6 +117,7 @@ inf_hauteur(Haut1,Haut2) :-
 	succ_hauteur(Haut1,Haut3),
 	inf_hauteur(Haut3,Haut2).
 
+/*inf_couleur : compare la hauteur de deux carte*/
 inf_couleur(Coul1,Coul2) :-
 	succ_couleur(Coul1,Coul2).
 
@@ -135,12 +137,6 @@ inf_carte(carte(Haut1,Coul1),carte(Haut1,Coul2)) :-
 	inf_couleur(Coul1,Coul2).
 	
 
-% ============================================================================= 
-%       QUESTION 3 : inf_carte_b(C1,C2) deuxième version
-% ==============================================================================
-%?
-
-
 % ==============================================================================
 %       QUESTION 4 : est_main_triee(main(C1,C2,C3,C4,C5))
 % ==============================================================================
@@ -157,31 +153,148 @@ est_main_triee(main(C1,C2,C3,C4,C5)) :-
 %       QUESTION 5 : une_paire(main(C1,C2,C3,C4,C5))
 % ==============================================================================
 
-
+trouve_paire(main(carte(Haut,_),carte(Haut,_),carte(H3,_),carte(H4,_),carte(H5,_)),Haut):-
+	\==(Haut,H3),
+	\==(Haut,H4),
+	\==(Haut,H5),
+	\==(H3,H4),
+	\==(H3,H5),
+	\==(H4,H5).
+	
+trouve_paire(main(carte(H1,_),carte(Haut,_),carte(Haut,_),carte(H4,_),carte(H5,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H4),
+	\==(Haut,H5),
+	\==(H1,H4),
+	\==(H1,H5),
+	\==(H4,H5).
+	
+trouve_paire(main(carte(H1,_),carte(H2,_),carte(Haut,_),carte(Haut,_),carte(H5,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H2),
+	\==(Haut,H5),
+	\==(H1,H2),
+	\==(H1,H5),
+	\==(H2,H5).
+	
+trouve_paire(main(carte(H1,_),carte(H2,_),carte(H3,_),carte(Haut,_),carte(Haut,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H2),
+	\==(Haut,H3),
+	\==(H1,H2),
+	\==(H3,H1),
+	\==(H2,H3).
+	
+une_paire(main(C1,C2,C3,C4,C5)):-
+	est_main_triee(main(C1,C2,C3,C4,C5)),
+	trouve_paire(main(C1,C2,C3,C4,C5),_).
 
 % ==============================================================================
 %       QUESTION 6 : deux_paires(main(C1,C2,C3,C4,C5))
 % ==============================================================================
-
+deux_paire(main(C1,C2,C3,C4,C5)):-
+	est_main_triee(main(C1,C2,C3,C4,C5)),
+	trouve_deux_paire(main(C1,C2,C3,C4,C5),Hautpaire1),
+	trouve_deux_paire(main(C1,C2,C3,C4,C5),Hautpaire2),
+	\==(Hautpaire1,Hautpaire2).
+	
+trouve_deux_paire(main(carte(Haut,_),carte(Haut,_),carte(H3,_),carte(H4,_),carte(H5,_)),Haut):-
+	\==(Haut,H3),
+	\==(Haut,H4),
+	\==(Haut,H5).
+	
+trouve_deux_paire(main(carte(H1,_),carte(Haut,_),carte(Haut,_),carte(H4,_),carte(H5,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H4),
+	\==(Haut,H5).
+	
+trouve_deux_paire(main(carte(H1,_),carte(H2,_),carte(Haut,_),carte(Haut,_),carte(H5,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H2),
+	\==(Haut,H5).
+	
+trouve_deux_paire(main(carte(H1,_),carte(H2,_),carte(H3,_),carte(Haut,_),carte(Haut,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H2),
+	\==(Haut,H3).
 
 
 % ============================================================================= 
 %       QUESTION 7 : brelan(main(C1,C2,C3,C4,C5))
 % ============================================================================= 
-
-
+brelan(main(C1,C2,C3,C4,C5)):-
+	est_main_triee(main(C1,C2,C3,C4,C5)),
+	trouve_brelan(main(C1,C2,C3,C4,C5),_).
+	
+trouve_brelan(main(carte(Haut,_),carte(Haut,_),carte(Haut,_),carte(H4,_),carte(H5,_)),Haut):-
+	\==(Haut,H4),
+	\==(Haut,H5),
+	\==(H4,H5).
+	
+trouve_brelan(main(carte(H1,_),carte(Haut,_),carte(Haut,_),carte(Haut,_),carte(H5,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H5),
+	\==(H1,H5).
+	
+trouve_brelan(main(carte(H1,_),carte(H2,_),carte(Haut,_),carte(Haut,_),carte(Haut,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H2),
+	\==(H1,H2).
+	
 
 
 % ============================================================================= 
 %       QUESTION 8 : suite(main(C1,C2,C3,C4,C5))
 % ==============================================================================
-
-
+suite(main(carte(C1,_),carte(C2,_),carte(C3,_),carte(C4,_),carte(C5,_))):-
+	est_main(main(carte(C1,_),carte(C2,_),carte(C3,_),carte(C4,_),carte(C5,_))),
+	succ_hauteur(C1,C2),
+	succ_hauteur(C2,C3),
+	succ_hauteur(C3,C4),
+	succ_hauteur(C4,C5).
+	
+	
 
 % ============================================================================= 
 %       QUESTION 9 : full(main(C1,C2,C3,C4,C5))
 % ============================================================================= 
-
+full(main(C1,C2,C3,C4,C5)):-
+	est_main_triee(main(C1,C2,C3,C4,C5)),
+	trouve_brelan_full(main(C1,C2,C3,C4,C5),Hautbrelan),
+	trouve_paire_full(main(C1,C2,C3,C4,C5),Hautpaire),
+	\==(Hautbrelan,Hautpaire).
+	
+trouve_paire_full(main(carte(Haut,_),carte(Haut,_),carte(H3,_),carte(H4,_),carte(H5,_)),Haut):-
+	\==(Haut,H3),
+	\==(Haut,H4),
+	\==(Haut,H5).
+	
+trouve_paire_full(main(carte(H1,_),carte(Haut,_),carte(Haut,_),carte(H4,_),carte(H5,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H4),
+	\==(Haut,H5).
+	
+trouve_paire_full(main(carte(H1,_),carte(H2,_),carte(Haut,_),carte(Haut,_),carte(H5,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H2),
+	\==(Haut,H5).
+	
+trouve_paire_full(main(carte(H1,_),carte(H2,_),carte(H3,_),carte(Haut,_),carte(Haut,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H2),
+	\==(Haut,H3).
+	
+trouve_brelan_full(main(carte(Haut,_),carte(Haut,_),carte(Haut,_),carte(H4,_),carte(H5,_)),Haut):-
+	\==(Haut,H4),
+	\==(Haut,H5).
+	
+trouve_brelan_full(main(carte(H1,_),carte(Haut,_),carte(Haut,_),carte(Haut,_),carte(H5,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H5).
+	
+trouve_brelan_full(main(carte(H1,_),carte(H2,_),carte(Haut,_),carte(Haut,_),carte(Haut,_)),Haut):-
+	\==(Haut,H1),
+	\==(Haut,H2).
 
 
 
@@ -242,18 +355,6 @@ No (0.11s cpu)
 % ============================================================================= 
 
 /* TESTS QUESTION 3 premiere version
-
-*/
-
-% ==============================================================================
-
-/* TESTS QUESTION 3 deuxieme version
-
-*/
-
-% ==============================================================================
-
-/* TESTS QUESTION 4
 ?- inf_carte(carte(X, Y), carte(cinq, coeur)).
 X = deux
 Y = trefle
@@ -298,12 +399,12 @@ X = cinq
 Y = carreau
 Yes (0.14s cpu, solution 14, maybe more)
 No (0.14s cpu)
-
 */
 
-% ============================================================================= 
+% ==============================================================================
 
-/* TESTS QUESTION 5
+/* TESTS QUESTION 4
+
 ?- main_test(X, Y), est_main_triee(Y).
 X = main_triee_une_paire
 Y = main(carte(sept, trefle), carte(valet, coeur), carte(dame, carreau), carte(dame, pique), carte(roi, pique))
@@ -330,31 +431,69 @@ No (0.02s cpu)
 
 */
 
+% ============================================================================= 
+
+/* TESTS QUESTION 5
+?- main_test(X, Y), une_paire(Y).
+X = main_triee_une_paire
+Y = main(carte(sept, trefle), carte(valet, coeur), carte(dame, carreau), carte(dame, pique), carte(roi, pique))
+Yes (0.06s cpu, solution 1, maybe more)
+X = m2
+Y = main(carte(valet, trefle), carte(valet, coeur), carte(dame, carreau), carte(roi, coeur), carte(as, pique))
+Yes (0.13s cpu, solution 2, maybe more)
+X = m2
+Y = main(carte(valet, carreau), carte(valet, coeur), carte(dame, carreau), carte(roi, coeur), carte(as, pique))
+Yes (0.13s cpu, solution 3, maybe more)
+No (0.13s cpu)
+*/
+
 % ==============================================================================
 
 /* TESTS QUESTION 6
-
-
+?- main_test(X, Y), deux_paire(Y).
+X = main_triee_deux_paires
+Y = main(carte(valet, trefle), carte(valet, coeur), carte(dame, carreau), carte(roi, coeur), carte(roi, pique))
+Yes (0.00s cpu, solution 1, maybe more)
+X = main_triee_deux_paires
+Y = main(carte(valet, trefle), carte(valet, coeur), carte(dame, carreau), carte(roi, coeur), carte(roi, pique))
+Yes (0.02s cpu, solution 2, maybe more)
+No (0.03s cpu)
 */
 
 % ==============================================================================
 
 
 /* TESTS QUESTION 7
-
+?- main_test(X, Y), brelan(Y).
+X = main_triee_brelan
+Y = main(carte(sept, trefle), carte(dame, carreau), carte(dame, coeur), carte(dame, pique), carte(roi, pique))
+Yes (0.00s cpu, solution 1, maybe more)
+No (0.00s cpu)
 
 */
 
 % ==============================================================================
 
 /* TESTS QUESTION 8
-
-
+?- main_test(X, Y), suite(Y).
+X = main_triee_suite
+Y = main(carte(sept, trefle), carte(huit, pique), carte(neuf, coeur), carte(dix, carreau), carte(valet, carreau))
+Yes (0.00s cpu, solution 1, maybe more)
+X = main_triee_suite
+Y = main(carte(sept, trefle), carte(huit, pique), carte(neuf, coeur), carte(dix, carreau), carte(valet, carreau))
+Yes (0.02s cpu, solution 2, maybe more)
+X = main_triee_suite
+Y = main(carte(sept, trefle), carte(huit, pique), carte(neuf, coeur), carte(dix, carreau), carte(valet, carreau))
+Yes (0.02s cpu, solution 3, maybe more)
+....
 */
 
 % ============================================================================= 
 
 /* TESTS QUESTION 9
-
-
+?- main_test(X, Y), full(Y).
+X = main_triee_full
+Y = main(carte(deux, coeur), carte(deux, pique), carte(quatre, trefle), carte(quatre, coeur), carte(quatre, pique))
+Yes (0.00s cpu, solution 1, maybe more)
+No (0.02s cpu)
 */
